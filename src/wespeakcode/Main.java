fa1090ce02cbb6b21bfa59ce58c8673c992ef9b9
Ilie Ciotir <dipixelhunter@gmail.com>
2016-07-02 16:59:28 +0300

    first init

---
+++ src/wespeakcode/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package wespeakcode;
+
+/**
+ * @author Ilie
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println(1 + 1);
+
+	}
+
+}
